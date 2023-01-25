874e5a184967caed632e1f34d25902a9009506bb
Kartone <estarpro1022@163.com>
2023-01-25 16:33:42 +0800

    array

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -5,20 +5,34 @@ public class ArrayDeque<T> {
 
     public ArrayDeque() {
         items = (T []) new Object[8];
-        nextFirst = 3;
-        nextLast = 4;
+        nextFirst = 7;
+        nextLast = 0;
         size = 0;
     }
 
+    private void ifResize() {
+        if (size == items.length) {
+            resize(2 * items.length);
+        }
+        if (items.length > 8) {
+            double ratio = 1.0 * size / items.length;
+            if (ratio <= 0.25) {
+                resize(items.length / 2);
+            }
+        }
+    }
+
     public void addFirst(T item) {
+        ifResize();
         items[nextFirst] = item;
-        nextFirst = nextFirst == 0 ? items.length - 1 : nextFirst - 1;
+        nextFirst--;
         size++;
     }
 
     public void addLast(T item) {
+        ifResize();
         items[nextLast] = item;
-        nextLast = nextLast == items.length - 1 ? 0 : nextLast + 1;
+        nextLast++;
         size++;
     }
 
@@ -32,13 +46,13 @@ public class ArrayDeque<T> {
 
     public void printDeque() {
         boolean[] isPrinted = new boolean[items.length];
-        for (int i = (nextFirst + 1) % items.length; i < items.length; i++) {
+        for (int i = nextFirst + 1; i < items.length; i++) {
             if (items[i] != null && !isPrinted[i]) {
                 System.out.print(items[i] + " ");
                 isPrinted[i] = true;
             }
         }
-        for (int i = 0; i <= (nextLast - 1) % items.length; i++) {
+        for (int i = 0; i < nextLast; i++) {
             if (items[i] != null && !isPrinted[i]) {
                 System.out.print(items[i] + " ");
                 isPrinted[i] = true;
@@ -48,14 +62,17 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
-        T item = items[(nextFirst + 1) % items.length];
-        items[(nextFirst + 1) % items.length] = null;
-        nextFirst = (nextFirst + 1) % items.length;
+        ifResize();
+        int firstItemPos = (nextFirst + 1) % items.length;
+        T item = items[firstItemPos];
+        items[firstItemPos] = null;
+        nextFirst = firstItemPos;
         size--;
         return item;
     }
 
     public T removeLast() {
+        ifResize();
         int lastItemPos = nextLast == 0 ? items.length - 1 : nextLast - 1;
         T item = items[lastItemPos];
         items[lastItemPos] = null;
@@ -65,9 +82,37 @@ public class ArrayDeque<T> {
     }
 
     public T get(int index) {
+        if (index < 0) {
+            System.out.println("Index is not allowed to be negative.");
+            return null;
+        }
+        if (index >= size) {
+            System.out.println("Index is out of bound.");
+            return null;
+        }
         return items[(nextFirst + 1 + index) % items.length];
     }
 
-
-
+    public void resize(int capacity) {
+        T[] newItems = (T []) new Object[capacity];
+        boolean[] isCopy = new boolean[items.length];
+        int num = 0;
+        for (int i = (nextFirst + 1) % items.length; i < items.length; i++) {
+            if (items[i] != null && !isCopy[i]) {
+                newItems[i - (nextFirst + 1) % items.length] = items[i];
+                num++;
+                isCopy[i] = true;
+            }
+        }
+        int lastItemPos = nextLast == 0 ? items.length - 1 : nextLast - 1;
+        for (int i = 0; i <= lastItemPos; i += 1) {
+            if (items[i] != null && !isCopy[i]) {
+                newItems[i + num] = items[i];
+                isCopy[i] = true;
+            }
+        }
+        items = newItems;
+        nextFirst = items.length - 1;
+        nextLast = size;
+    }
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,17 +1,83 @@
 public class ArrayDequeTest {
     public static void main(String[] args) {
         System.out.println("Running tests.\n");
+//        Test1();
+//        Test2();
+        Test3();
+    }
+
+    public static void Test1() {
+        System.out.println("Test1:");
         ArrayDeque<Integer> a = new ArrayDeque<>();
         a.addFirst(1);
         a.addFirst(2);
         a.addLast(3);
         a.addLast(4);
-        System.out.print("ArrayDeque status: ");
+        System.out.println("No resize: ");
+        printBasicInfo(a);
+        System.out.println("Index 1: " + a.get(1));
+        System.out.println();
+    }
+
+    public static void Test2() {
+        System.out.println("Test2: resize for bigger");
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        printBasicInfo(a);
+        a.addFirst(1);
+        a.addFirst(2);
+        a.addFirst(3);
+        a.addFirst(4);
+        a.addFirst(5);
+        a.addFirst(6);
+        a.addFirst(7);
+        a.addFirst(8);
+        printBasicInfo(a);
+        System.out.println("addLast");
+        a.addLast(9);
+        a.addLast(10);
+        a.addLast(11);
+        printBasicInfo(a);
+        System.out.println("removeFirst");
+        a.removeFirst();
+        a.removeFirst();
+        printBasicInfo(a);
+        System.out.println("removeLast");
+        a.removeLast();
+        a.removeLast();
+        printBasicInfo(a);
+        System.out.println("Index 5: " + a.get(5));
+    }
+
+    public static void Test3() {
+        ArrayDeque<Integer> a = new ArrayDeque<>();
+        a.addFirst(1);
+        a.addFirst(2);
+        a.addFirst(3);
+        a.addFirst(4);
+        a.addFirst(5);
+        a.addFirst(6);
+        a.addFirst(7);
+        a.addFirst(8);
+        a.addFirst(9);
+        a.addFirst(10);
+        printBasicInfo(a);
+        System.out.println("resize for smaller");
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        a.removeLast();
+        printBasicInfo(a);
+        System.out.println();
+    }
+
+    public static void printBasicInfo(ArrayDeque<Integer> a) {
+        System.out.print("Items: ");
         a.printDeque();
         System.out.println("size: " + a.size());
-
-        boolean passed = checkEmpty(a.isEmpty(), false) && checkSize(a.size(), 4);
-        printTestStatus(passed);
+        System.out.println("If empty? " + a.isEmpty());
     }
 
     public static boolean checkEmpty(boolean expected, boolean actual) {
